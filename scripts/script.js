17ca54b0474b3efe5f10eb7bc365ca66ae540d34
DevRimsha <coderimsha@gmail.com>
2024-12-06 05:55:15 +0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+console.log("Welcome to my Resume Builder!");
+function greet(name) {
+    return "Hello, ".concat(name, "! Welcome to my resume.");
+}
+console.log(greet("Visitor"));
---
+++ scripts/script.ts
@@ -0,0 +1,6 @@
+console.log("Welcome to my Resume Builder!");
+
+function greet(name: string): string {
+  return `Hello, ${name}! Welcome to my resume.`;
+}
+console.log(greet("Visitor"));
